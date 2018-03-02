1abf90febc0e35f42bbf49f7e4e89465b5dbaacf
qilonghuafeng <1440176729@qq.com>
2018-03-02 21:06:54 +0800

    阳刚之神功，至深之处刚柔相济

---
+++ src/gongfu/Jiuyangshengong.java
@@ -0,0 +1,22 @@
+package gongfu;
+
+public class Jiuyangshengong {
+	private String id;
+	private String name;
+	private String property;
+	public Jiuyangshengong() {
+		super();
+	}
+	public Jiuyangshengong(String id, String name, String property) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.property = property;
+	}
+	@Override
+	public String toString() {
+		return "Jiuyangshengong [id=" + id + ", name=" + name + ", property=" + property + "]";
+	}
+	
+
+}
